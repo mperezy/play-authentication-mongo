72a21a62d832d7cca93ca17de47cba6cdd61a29a
Manuel Pérez <mperezy@unitru.edu.pe>
2019-02-07 02:05:37 -0500

    Working on template views

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,21 +3,26 @@ package controllers
 import forms.{SignInForm, SignUpForm}
 import play.api.mvc._
 import jp.t2v.lab.play2.auth.AuthenticationElement
+import models.Account
+import org.joda.time.DateTime
+import reactivemongo.bson.BSONObjectID
 import services.AuthConfigImpl
 
 import scala.concurrent.Future
 
 object Application extends Controller with AuthenticationElement with AuthConfigImpl {
 
+  val emptyAccount = new Account(Some(BSONObjectID.generate), Some(""), "", Some(""), Some(""), "", Some(""), Some(new DateTime()))
+
   def showSignInForm = Action.async { implicit request =>
     request.session.get("rememberme") match {
       case Some(string) => Future.successful(Redirect(routes.Application.showDashboard()))
-      case _ => Future.successful(Ok(views.html.signIn(SignInForm.form)))
+      case _ => Future.successful(Ok(views.html.signIn(SignInForm.form, emptyAccount)))
     }
   }
 
   def showSignUpForm = Action.async { implicit request =>
-    Future.successful(Ok(views.html.signUp(SignUpForm.form)))
+    Future.successful(Ok(views.html.signUp(SignUpForm.form, emptyAccount)))
   }
 
   def showDashboard = StackAction { implicit request =>
